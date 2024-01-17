0c81e9028ffc5891ed135cbcf3d90d5a7957fb79
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-01-16 22:06:37 -0300

    Añadidos algunos métodos adicionales

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function validarStringNoNumber(string) {
 
 //Información
 const baseDeDatos = [
-  { nombre: "TV", precio: 8463, stock: 5 },
+  { nombre: "Tv", precio: 8463, stock: 5 },
   { nombre: "Monitor", precio: 7338, stock: 27 },
   { nombre: "Notebook", precio: 4390, stock: 23 },
   { nombre: "Licuadora", precio: 3869, stock: 16 },
@@ -31,9 +31,9 @@ const baseDeDatos = [
   { nombre: "Auriculares", precio: 337, stock: 20 },
   { nombre: "Procesador", precio: 2643, stock: 3 },
   { nombre: "MousePad", precio: 6743, stock: 28 },
-  { nombre: "Placa Gráfica", precio: 4812, stock: 4 },
+  { nombre: "Amplificador", precio: 4812, stock: 4 },
   { nombre: "Llantas", precio: 7112, stock: 0 },
-  { nombre: "Heladera Inteligente", precio: 9376, stock: 4 },
+  { nombre: "Freezer", precio: 9376, stock: 4 },
   { nombre: "Podadora", precio: 5748, stock: 0 },
   { nombre: "Zapatillas", precio: 1381, stock: 17 },
   { nombre: "Pantalones", precio: 3531, stock: 11 },
@@ -43,23 +43,23 @@ const baseDeDatos = [
 
 
 //Bienvenida
-
-console.log(`Bienvenido a nuestra tienda!
-Tenemos a la venta los siguientes productos:`)
+console.log('Bienvenido a nuestra tienda!')
+console.group('Tenemos a la venta los siguientes productos:')
 console.table(baseDeDatos)
+console.groupEnd()
 
 
 //Código
 
 class Carrito {
   constructor() {
-    this.esteCarrito = []
+    this.productos = []
   }
 
   agregarProducto() {
     // Recibir y formatear la entrada
     do {
-      this.productoTemporal = validarStringNoNumber(prompt("Ingrese un elemento para añadir al carrito:"))
+      this.productoTemporal = validarStringNoNumber(prompt("Ingrese un producto para añadir al carrito:"))
     } while (this.productoTemporal === undefined)
 
     this.productoTemporal = String(this.productoTemporal).toLowerCase()
@@ -79,6 +79,7 @@ class Carrito {
 
     if (!stock) {
       console.log('El producto seleccionado está fuera de stock, elija otro por favor')
+      alert('El producto seleccionado está fuera de stock, elija otro por favor')
     }
 
     //Después de cumplir las dos condiciones, añado el producto al carrito
@@ -88,30 +89,46 @@ class Carrito {
       let indexProducto = baseDeDatos.findIndex(elem => elem.nombre === productoCase)
       let productoFinal = { ...baseDeDatos[indexProducto] }
       delete productoFinal.stock
-      this.esteCarrito.push(productoFinal)
-      console.log(`Producto añadido al carrito!`)
+      this.productos.push(productoFinal)
+      alert(`Producto añadido al carrito!`)
+      return `${productoCase} añadido al carrito!`
     }
 
   }
 
-  verCarrito() {
-    console.log(this.esteCarrito)
-  }
+  verProductos() {
 
-  seachElementsInStore() {
+    if (this.productos.length === 0) {
+      return 'Tu carrito está vacío!'
+    }
 
+    console.group(`Estos son los productos que están actualmente en tu carrito:`)
+    console.table(this.productos)
+    console.groupEnd()
   }
 
   eliminarProducto() {
   }
 
-  mostrarProductos() {
+  calcularTotal() {
+    let suma = 0;
+    this.productos.forEach(product => suma += product.precio)
+    this.verProductos()
+    console.log(`El total de tu carrito es: $${suma}`)
   }
 
-  getElementById() {
+  verListado() {
+    console.log('Estos son todos nuestros productos:')
+    console.table(baseDeDatos)
+    return 'Estos son todos nuestros productos:';
   }
 
-  getElementByName() {
+  buscarProductos(producto) {
+    const entrada = producto.toLowerCase()
+    const answer = baseDeDatos.filter(elemento => elemento.nombre.toLowerCase().includes(entrada))
+    console.group('Este es el resultado de la búsqueda:')
+    console.table(answer)
+    console.groupEnd()
   }
 
 
@@ -119,11 +136,7 @@ class Carrito {
 }
 
 
-
-
-
-
-let carrito1 = new Carrito();
+let carrito = new Carrito();
 
 
 
