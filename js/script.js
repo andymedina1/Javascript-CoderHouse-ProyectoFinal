42fa401975273a42bcf84af0113bdbfe4c070b07
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-02-07 02:41:57 -0300

    Mejorado el órden del código JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-// Elementos del DOM
+/*    Elementos del DOM   */
 
 const container = document.querySelector('#middleContainer')
 const botonAbrirCarrito = document.querySelector('#botonCarrito')
@@ -14,7 +14,8 @@ const numeroCarrito = document.querySelector('#numeroCarrito')
 const botonVaciarCarrito = document.querySelector('#vaciarCarrito')
 const botonComprarCarrito = document.querySelector('#comprarCarrito')
 
-// Información
+/*    Información   */
+
 const baseDeDatos = [
   { id: 0, nombre: 'Tv', precio: 8463, categoria: 'pantallas', cantidad: 1, img: 'img/tv.jpg' },
   { id: 1, nombre: 'Monitor', precio: 3387, categoria: 'pantallas', cantidad: 1, img: 'img/monitor.jpg' },
@@ -28,11 +29,8 @@ const baseDeDatos = [
   { id: 9, nombre: 'Memoria', precio: 6743, categoria: 'complementos', cantidad: 1, img: 'img/memoria.jpg' }
 ]
 
-// Botón del Carrito
-botonAbrirCarrito.addEventListener('click', () => {
-  fillCart()
-  modal.show()
-})
+
+/*    Funciones   */
 
 // Función para mostrar los productos en pantalla
 function renderProducts(data) {
@@ -53,59 +51,44 @@ function renderProducts(data) {
       </div>
       `
   })
-  agregarMaestro()
+  setButtons()
 }
 
-// Función del buscador
-buscador.addEventListener('input', (e) => {
-  const busqueda = e.target.value.toLowerCase()
-  const resultado = baseDeDatos.filter(producto =>
-    producto.nombre.toLowerCase().includes(busqueda)
-  )
-  renderProducts(resultado)
-})
-
-// Función para filtrar
-filtro.addEventListener('input', (e) => {
-  const input = e.target.value
-  const resultado = baseDeDatos.filter(producto =>
-    producto.categoria.includes(input)
-  )
-  renderProducts(resultado)
-})
-
-// Función para Agregar al carrito desde botón de tarjeta
-function agregarMaestro() {
+// Función para configurar botones de Agregar al carrito cada vez que renderizo
+function setButtons() {
   // obtengo los botones
   const botonesAgregar = document.querySelectorAll('.agregarCarrito')
 
   // Le agrego la función a cada uno
   botonesAgregar.forEach(btn => {
     btn.addEventListener('click', (btn) => {
-      agregar(btn)
+      agregarProducto(btn)
     })
   })
 
-  // Función para agregar los productos al carrito y guardar en LocalStorage
-  function agregar(btn) {
-    const id = btn.target.id
-    const index = baseDeDatos.findIndex(producto => producto.id == id)
-    const producto = baseDeDatos[index]
+}
 
-    // Verifico si el producto está en el carrito
-    if (esteCarrito.some(prod => prod.id == producto.id)) {
-      // Si el producto ya está en el carrito, solo aumento la cantidad
-      const dice = esteCarrito.findIndex(prod => prod.id == producto.id)
-      esteCarrito[dice].cantidad += 1
-    } else {
-      // Si el producto NO está en el carrito, añado el nuevo producto
-      esteCarrito.push({ ...baseDeDatos[index] })
-    }
+// Función para agregar los productos al carrito y guardar en LocalStorage
+function agregarProducto(btn) {
+  const botonId = btn.target.id
+  const index = baseDeDatos.findIndex(producto => producto.id == botonId)
+  const productoSeleccionado = baseDeDatos[index]
 
-    localStorage.setItem('carrito', JSON.stringify(esteCarrito))
-
-    cartNumber()
+  // Verifico si el producto está en el carrito
+  if (esteCarrito.some(prod => prod.id == productoSeleccionado.id)) {
+    // Si el producto ya está en el carrito, solo aumento la cantidad
+    const indexCarrito = esteCarrito.findIndex(prod => prod.id == productoSeleccionado.id)
+    esteCarrito[indexCarrito].cantidad += 1
+  } else {
+    // Si el producto NO está en el carrito, añado el nuevo producto
+    esteCarrito.push({ ...baseDeDatos[index] })
   }
+
+  // Guardo el carrito
+  localStorage.setItem('carrito', JSON.stringify(esteCarrito))
+
+  // Actualizo el badge
+  cartNumber()
 }
 
 // Función para cargar los productos en el carrito
@@ -128,7 +111,7 @@ function fillCart() {
   modalTotal.innerHTML += `Total: $${totalFinal}`
 }
 
-// Cambiar número del carrito
+// Función para cambiar número del carrito
 function cartNumber() {
   let count = 0
   const carrito = JSON.parse(localStorage.getItem('carrito'))
@@ -146,9 +129,6 @@ function vaciarCarrito() {
   modal.hide()
 }
 
-// Botón para vaciar el carrito
-botonVaciarCarrito.addEventListener('click', vaciarCarrito)
-
 // Función para comprar el carrito
 function comprarCarrito() {
   modal.hide()
@@ -162,8 +142,51 @@ function comprarCarrito() {
   vaciarCarrito()
 }
 
+
+
+/*    Eventos   */
+
+// Input del buscador
+buscador.addEventListener('input', (e) => {
+  const busqueda = e.target.value.toLowerCase()
+  const resultado = baseDeDatos.filter(producto =>
+    producto.nombre.toLowerCase().includes(busqueda)
+  )
+  renderProducts(resultado)
+})
+
+// Input para filtrar
+filtro.addEventListener('input', (e) => {
+  const seleccion = e.target.value
+  const resultado = baseDeDatos.filter(producto =>
+    producto.categoria.includes(seleccion)
+  )
+  renderProducts(resultado)
+})
+
+// Botón para mostrar el Carrito
+botonAbrirCarrito.addEventListener('click', () => {
+  fillCart()
+
+  // Si el carrito está vacío, deshabilito el botón para vaciar
+  if (esteCarrito.length == 0) {
+    botonVaciarCarrito.setAttribute("disabled", "")
+  } else {
+    botonVaciarCarrito.removeAttribute("disabled")
+  }
+
+  modal.show()
+})
+
+// Botón para vaciar el carrito
+botonVaciarCarrito.addEventListener('click', vaciarCarrito)
+
 // Botón para comprar el carrito
 botonComprarCarrito.addEventListener('click', comprarCarrito)
+
+
+
+/*    Inicialización   */
 
 // Primero muestro todos los productos
 renderProducts(baseDeDatos)
